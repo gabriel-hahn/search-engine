b521128637c2ab15b3cf03a44dd6e851b0078249
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-10-31 21:36:45 -0300

    getByTerm method to search sites

--- api/src/controllers/SiteController.js
+++ api/src/controllers/SiteController.js
@@ -9,5 +9,27 @@ module.exports = {
     async getSiteByUrl(req, res) {
         let sites = await Site.find({ 'url': req.body.url });
         res.json(sites);
+    },
+    async getByTerm(req, res) {
+        let term = req.params.term;
+        let sites = await Site.find(
+            {
+                $or: [
+                    {
+                        'url': { '$regex': term, '$options': 'i' }
+                    },
+                    {
+                        'title': { '$regex': term, '$options': 'i' }
+                    },
+                    {
+                        'description': { '$regex': term, '$options': 'i' }
+                    },
+                    {
+                        'keywords': { '$regex': term, '$options': 'i' }
+                    }
+                ]
+            });
+
+        res.json(sites);
     }
 };
--- api/src/routes/SiteRoutes.js
+++ api/src/routes/SiteRoutes.js
@@ -3,4 +3,5 @@ const SiteController = require('../controllers/SiteController');
 module.exports = app => {
     app.post('/api/site', SiteController.insertSite),
     app.post('/api/site/siteByUrl', SiteController.getSiteByUrl);
+    app.get('/api/site/getByTerm/:term', SiteController.getByTerm);
 }
