ca91876c153312c6ab7bf5b97026b47ffbcede2c
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-11-02 11:58:54 -0300

    Image getByTerm method

--- api/src/controllers/ImagesController.js
+++ api/src/controllers/ImagesController.js
@@ -3,11 +3,33 @@ const Image = mongoose.model('Image');
 
 module.exports = {
     async insertImage(req, res) {
-        let site = await Image.create(req.body);
-        res.json(site);
+        let image = await Image.create(req.body);
+        res.json(image);
     },
     async getImageByImageUrl(req, res) {
         let images = await Image.find({ 'imageUrl': req.body.imageUrl });
+        res.json(images);
+    },
+    async getByTerm(req, res) {
+        let term = req.params.term;
+        let images = await Image.find(
+            {
+                $or: [
+                    {
+                        'siteUrl': { '$regex': term, '$options': 'i' }
+                    },
+                    {
+                        'imageUrl': { '$regex': term, '$options': 'i' }
+                    },
+                    {
+                        'alt': { '$regex': term, '$options': 'i' }
+                    },
+                    {
+                        'title': { '$regex': term, '$options': 'i' }
+                    }
+                ]
+            });
+
         res.json(images);
     }
 };
--- api/src/routes/ImagesRoutes.js
+++ api/src/routes/ImagesRoutes.js
@@ -3,4 +3,5 @@ const ImagesController = require('../controllers/ImagesController');
 module.exports = app => {
     app.post('/api/image', ImagesController.insertImage),
     app.post('/api/image/imageByUrl', ImagesController.getImageByImageUrl);
+    app.get('/api/image/getByTerm/:term', ImagesController.getByTerm);
 }
