ea8975597480e9f5b2bde9420143651297042ea9
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-01-23 15:19:40 -0200

    Update mongodb.js

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -4,7 +4,7 @@ const requireDir = require('require-dir');
 const dbName = 'searchEngine';
 const url = `mongodb://localhost:27017/${dbName}`;
 
-//In this case, I keep the connect with MongoDB opens, but in a "professional" environment, the connection only will be open each request.
+//In this case, I keep the connect with MongoDB opens, but in a production environment, the connection only will be open each request.
 mongoose.Promise = global.Promise;
 mongoose.connect(url, { useNewUrlParser: true });
 requireDir('../models');
